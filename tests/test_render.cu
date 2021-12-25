a993652b8f0b69a7504908c20789f46c6c5cfd81
kajananchinniah <kajananchinniah@gmail.com>
2021-12-25 16:17:44 -0500

    Removed unnecessary header and moved includes to test

--- tests/test_render.cu
+++ tests/test_render.cu
@@ -1,4 +1,10 @@
-#include "test_render.hpp"
+#include "common/common_types.hpp"
+#include "image_utils/image.hpp"
+#include "image_utils/image_utils.hpp"
+#include "vector3/vector3.hpp"
+
+#include "cuda.h"
+#include "cuda_runtime.h"
 
 namespace
 {
